dbbfb8009b3496f5d5763c1a58ef312dedee1619
Luthov <lukettl15@gmail.com>
2024-10-24 22:52:44 +0800

    added execution in moveit

--- bite_acquisition/scripts/mujoco_action_server.py
+++ bite_acquisition/scripts/mujoco_action_server.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python
 
 import numpy as np
-import time
 from scipy.spatial.transform import Rotation as R
+import time
 
 import rospy
+import rospkg
 import actionlib
 from geometry_msgs.msg import PoseStamped
-from moveit_msgs.msg import RobotTrajectory
+from moveit_msgs.msg import RobotTrajectory, MoveItErrorCodes
 from moveit_msgs.msg import ExecuteTrajectoryAction, ExecuteTrajectoryGoal
+
 from bite_acquisition.msg import mujoco_action_serverAction, mujoco_action_serverResult, mujoco_action_serverFeedback
 
 try:
@@ -31,8 +33,10 @@ class MujocoAction(object):
         controller_config = load_controller_config(default_controller="IK_POSE")
         controller_config["control_delta"] = False
 
+        model_folder = rospkg.RosPack().get_path("feeding_mujoco") + "/src/feeding_mujoco/models"
         env_config = {
-            "model_path": "/home/luthov_ubuntu/School/FYP/learning_mujoco/models/envs/mujoco_scooping_test/feeding.xml",
+            "model_path": model_folder + "/envs/mujoco_scooping_test/feeding.xml",
+            # "model_path": model_folder + "/robots/xarm6/xarm6_with_ft_sensor_gripper_with_spoon.xml",
             "sim_timestep": 0.002,
             "controller_config": controller_config,
             "control_freq": 100,
@@ -56,26 +60,43 @@ class MujocoAction(object):
         )
 
         self.planner = MoveItPlanner(dt = 1/self.env._policy_freq)
-                
+
         # Step the simulator to update the robot and environment state
         self.env.sim.step()
 
+        # Sync moveit robot state with mujoco robot state (must be done after step)
+        self._action_name_arm_exec = "/execute_trajectory"
+        initial_eef_pose = self.env._robot.get_eef_pose()
+        msg_initial_eef_pose = PoseStamped()
+        msg_initial_eef_pose.header.frame_id = "world"
+        msg_initial_eef_pose.pose.position.x = initial_eef_pose[0]
+        msg_initial_eef_pose.pose.position.y = initial_eef_pose[1]
+        msg_initial_eef_pose.pose.position.z = initial_eef_pose[2]
+        msg_initial_eef_pose.pose.orientation.w = initial_eef_pose[3]
+        msg_initial_eef_pose.pose.orientation.x = initial_eef_pose[4]
+        msg_initial_eef_pose.pose.orientation.y = initial_eef_pose[5]
+        msg_initial_eef_pose.pose.orientation.z = initial_eef_pose[6]
+
+        msg_joint_traj = self.planner.get_motion_plan(start_joint_position=[0, 0, 0, 0, 0, 0], 
+                                                      goal=msg_initial_eef_pose, 
+                                                      goal_type="pose_goal", 
+                                                      velocity_scaling_factor=1.0)
+        self.execute_traj_in_moveit(msg_joint_traj)
+        rospy.loginfo(f"Moved moveit robot to initial pose: {self.env._robot.get_eef_pose()}")
+        
         self.acq_pos = np.radians([0.0, -65.0, -25.0, 0.0, 65.0, -90.0])
         self.transfer_pos = np.radians([0.0, -65.0, -25.0, 0.0, 0.0, -90.0])
-        self.cutting_pos = np.radians([0.0, -65.0, -25.0, 0.0, 90.0, 0.0])
 
         self.action_name = "mujoco_action_server"
         self.action_server = actionlib.SimpleActionServer(self.action_name, mujoco_action_serverAction, execute_cb=self.execute_callback, auto_start = False)
         self.action_server.start()
 
-        # Create an action client for the ExecuteTrajectory action and wait for the server
-        self.moveit_client = actionlib.SimpleActionClient('execute_trajectory', ExecuteTrajectoryAction)
-        self.moveit_client.wait_for_server()
-
+     
     def execute_callback(self, goal):
 
         try:
             if goal.function_name == "move_to_pose":
+                print("Moving to pose")
                 self.move_to_pose(goal.goal_point)
             elif goal.function_name == "move_to_acq_pose":
                 self.move_to_acq_pose()
@@ -152,8 +173,6 @@ class MujocoAction(object):
                     pos_error = y[:3] - self.env._robot.get_eef_pose()[:3]
                     pos_error_norm = np.linalg.norm(pos_error)                        
                     orn_error = np.linalg.norm(quat_error(self.env._robot.get_eef_pose()[3:], y[3:]))
-                    # print("pos_error:", pos_error)
-                    # print("orn_error:", orn_error)
 
                     # Break if the error is not reducing
                     if self.env._sim.time - sim_time > 2.0:
@@ -161,38 +180,15 @@ class MujocoAction(object):
                         break
 
                 correction_time = self.env._sim.time - sim_time
+                # print("Correction time:", correction_time, "steps:", int(correction_time/env._sim_timestep))
 
-                # print("Final joint pose:", joint_position)
-                # print("final eef pose:", self.env._robot.get_eef_pose())
+                print("Final joint pose:", joint_position)
+                print("final eef pose:", self.env._robot.get_eef_pose())
                 break
+                # print("Moved to pose successfuly!")
+                # break
 
-    def moveit_execution(self, msg_robot_trajectory):
-
-        # Create a goal message
-        goal = ExecuteTrajectoryGoal()
-
-        # Assign the trajectory to the goal
-        goal.trajectory = msg_robot_trajectory
-
-        # Send the goal to the action server
-        self.moveit_client.send_goal(goal)
-
-        # Wait for the result (blocking call)
-        self.moveit_client.wait_for_result()
-
-        # Check the result of the execution
-        result = self.moveit_client.get_result()
-
-        if result:
-            rospy.loginfo("MoveIt Trajectory executed successfully!")
-        else:
-            rospy.logerr("MoveIt Failed to execute trajectory.")
-        
-        return result
-    
     def set_joint_position(self, joint_position):
-
-        success = False
 
         # Reset the planner
         self.planner.reset()
@@ -211,17 +207,21 @@ class MujocoAction(object):
         )
 
         if msg_trajectory is not None:
-            success = self.moveit_execution(msg_trajectory)
             self.execute_trajectory(msg_trajectory)
 
     def rotate_eef(self, angle):
         target_joint_positions = self.env._robot._joint_positions
+
+        print(f"initial_joint_positions: {target_joint_positions}")
+
         target_joint_positions[5] += angle
+
+        print(f"target_joint_positions: {target_joint_positions}")
+
         self.set_joint_position(target_joint_positions)
 
     def move_to_pose(self, pose):
-        
-        success = False
+
         # Reset the planner
         self.planner.reset()
 
@@ -249,19 +249,58 @@ class MujocoAction(object):
             velocity_scaling_factor=0.1
         )
 
-        if msg_trajectory is not None:     
-            success = self.moveit_execution(msg_trajectory)
+        if msg_trajectory is not None:
+            print("moveit exec trajectory...")
             self.execute_trajectory(msg_trajectory)
                 
     def move_to_acq_pose(self):
-        # self.set_joint_position(self.acq_pos)
-        self.set_joint_position(self.cutting_pos)
+        self.set_joint_position(self.acq_pos)
 
     def move_to_transfer_pose(self):
         self.set_joint_position(self.transfer_pos)
 
     def reset(self):
         self.move_to_acq_pose()
+
+    def execute_traj_in_moveit(self, msg_robot_traj):
+        start_time = time.time()
+        success = True
+        # Execute the scooping trajectory
+        client = actionlib.SimpleActionClient(self._action_name_arm_exec, ExecuteTrajectoryAction)
+        rospy.loginfo(f"Waiting for action server... {self._action_name_arm_exec}")
+        client.wait_for_server()
+        rospy.loginfo(f"Action server {self._action_name_arm_exec} is up!")
+
+        # Create goal
+        goal = ExecuteTrajectoryGoal()
+        goal.trajectory = msg_robot_traj
+        goal.trajectory.joint_trajectory.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
+
+        # Send goal to server
+        client.send_goal(goal)
+        client.wait_for_result()
+
+        # Check if action was successful or preempted
+        if client.get_state() == actionlib.GoalStatus.PREEMPTED:
+            rospy.loginfo("Moveit robot trajectory execution preempted")
+            success = False
+
+        elif client.get_state() == actionlib.GoalStatus.SUCCEEDED:
+            result = client.get_result()
+            
+            # Process the result
+            if result.error_code.val != MoveItErrorCodes.SUCCESS:
+                error_dict = MoveItErrorCodes.__dict__
+                error_string = list(error_dict.keys())[list(error_dict.values()).index(result.error_code.val)]
+                rospy.logwarn(f"Moveit robot execution failed with error code {result.error_code.val}: {error_string}")
+            else:
+                rospy.loginfo(f"Moveit robot trajectory executed successfully in {time.time() - start_time:.3f}(s)")
+                
+        else:
+            rospy.loginfo(f"Moveit robot trajectory failed or was aborted with status {client.get_state()}.")
+            success = False
+
+        return success
 
 if __name__ == '__main__':
     rospy.init_node('mujoco_action_server', anonymous=True)
